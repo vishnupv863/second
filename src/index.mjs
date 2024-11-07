bb9ed197ee6ce216e88a555fe3541556978909e6
vishnupv863 <vishnupv863@gmail.com>
2024-11-07 23:33:35 +0530

    7th nov

--- src/index.mjs
+++ src/index.mjs
@@ -33,11 +33,11 @@ app.post("/api/users", (req,res) => {
 
 app.put("/api/users/:id", (req,res) => {
     const {params:{id}, body} = req;
-    const parseId = parseInt(id);
-    const findIndex = mockUsers.findIndex((user) => user.id===parseId);
-    if(findIndex ===-1) return res.sendStatus(404);
-    mockUsers[findIndex] = {id:parseId, ...body}
-    return res.send(mockUsers)
+    const parsedId = parseInt(id);
+    const findIndex = mockUsers.findIndex((user) => user.id === parsedId);
+    if(findIndex === -1) return res.status(404).send("user not found");
+    mockUsers[findIndex] = {id:parsedId, ...body}
+    return res.status(200).send(mockUsers);
 });
 
 app.get("/api/products", (req, res) => {
